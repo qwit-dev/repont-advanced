ce9cb34a0a9e44eae70c8410ae53baea799a6cec
BalazsManus <balazsmanus@proton.me>
2024-10-03 20:48:33 +0200

    make map resize on update

--- src/ui/main/pages/index.tsx
+++ src/ui/main/pages/index.tsx
@@ -1,18 +1,32 @@
-// leaflet
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { useState, useEffect } from 'react';
+import { MapContainer, TileLayer, Marker, useMap } from 'react-leaflet';
+
+const ResizeHandler = () => {
+    const map = useMap();
+    const [height, setHeight] = useState(window.innerHeight);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setHeight(window.innerHeight);
+            map.invalidateSize();
+        };
+        window.addEventListener('resize', handleResize);
+
+        return () => window.removeEventListener('resize', handleResize);
+    }, [map]);
+
+    return null;
+}
 
 const MainIndexPage = () => {
     return (
-        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{ height: "500px" }}>
+        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true} style={{ height: '100vh' }}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={[51.505, -0.09]}>
-                <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-            </Marker>
+            <Marker position={[51.505, -0.09]} />
+            <ResizeHandler />
         </MapContainer>
     );
 }
