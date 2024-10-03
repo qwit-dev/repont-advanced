7bf69361618a9b8be9dc3d1cb1f68ed448bc242c
BalazsManus <balazsmanus@proton.me>
2024-10-03 20:26:23 +0200

    vegre geci

--- src/ui/main/pages/index.tsx
+++ src/ui/main/pages/index.tsx
@@ -1,11 +1,19 @@
 // leaflet
-import { MapContainer, TileLayer, useMap } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 const MainIndexPage = () => {
     return (
-        <div>
-            <p>refasz</p>
-        </div>
+        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{ height: "500px" }}>
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[51.505, -0.09]}>
+                <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+            </Marker>
+        </MapContainer>
     );
 }
 
